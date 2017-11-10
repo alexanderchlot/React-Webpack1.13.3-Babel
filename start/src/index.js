06e911d8b005f75ee36424da22605f4fd4d6ae34
Alexander <alexanderchlot@gmail.com>
2017-11-10 11:23:13 +0100

    first commit

---
+++ start/src/index.js
@@ -0,0 +1,10 @@
+const { render } = ReactDOM
+
+render(
+	<h1 id="title"
+	className="header"
+	style={{backgroundColor: 'black', color: 'white', fontFamily: 'verdana'}}>
+	Alexander Chlot
+	</h1>,
+	document.getElementById('react-container')
+)
---
+++ start/webpack.config.js
@@ -0,0 +1,26 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+    entry: path.resolve(__dirname, './src/index'),
+    output: {
+        path: path.resolve(__dirname, './dist/assets'),
+        filename: 'bundle.js',
+        publicPath: 'assets'
+    },
+    devServer: {
+        inline: true,
+        contentBase: path.resolve(__dirname, './dist'),
+        port: 3000
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            exclude: /(node_modules)/,
+            loader: path.resolve(__dirname, './node_modules/babel-loader'),
+            query: {
+                presets: ['latest', 'stage-0', 'react']
+            }
+        }]
+    }
+}
